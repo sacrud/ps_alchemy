c811cd4195abc67536be2d2e040e994ae384abb1
Dmitry <root@uralbash.ru>
2015-12-10 13:12:50 +0500

    add model preparing

--- ps_alchemy/__init__.py
+++ ps_alchemy/__init__.py
@@ -5,9 +5,30 @@
 # Copyright © 2015 uralbash <root@uralbash.ru>
 #
 # Distributed under terms of the MIT license.
+from pyramid.events import ApplicationCreated
+from pyramid_sacrud import CONFIG_MODELS
+from pyramid_sacrud.resources import GroupResource
+
+from .resources import ListResource
+
+
+def models_preparing(app):
+    """ Wrap all sqlalchemy model in settings.
+    """
+    settings = app.app.registry.settings
+    models = settings[CONFIG_MODELS]
+
+    def wrapper(resource, parent):
+        return ListResource(resource, parent=parent)
+
+    models = [(k, [wrapper(r, GroupResource(k, v)) for r in v])
+              for k, v in models]
+    settings[CONFIG_MODELS] = models
 
 
 def includeme(config):
+    config.add_subscriber(models_preparing, ApplicationCreated)
+
     config.include('pyramid_jinja2')
     config.add_jinja2_search_path("templates")
     config.add_static_view('/static', 'ps_alchemy:static')
